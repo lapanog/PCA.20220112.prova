3716cb42bf6bf5053efa1b5d22e9c5320cd982f1
lapanog <120739086+lapanog@users.noreply.github.com>
2023-01-17 16:57:17 -0400

    Create 20220112.SI02.c

---
+++ 20220112.SI02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int funcao1(float x) {
+    float num = 0; x = 0;
+    printf("digite um numero real: ");
+    scanf("%f" &num);
+    if(num > 0 && num < 1) {
+        x = num;
+    }
+    return num;
+}
+
+int arctan(float aprox) {
+    x = funcao1(x);
+    
+}
+
+int funcao3(float x, char ) {
+    
+}
+
+
+int main () {
+    int a;
+    imprimir = funcao3(a);
+    printf
+}
